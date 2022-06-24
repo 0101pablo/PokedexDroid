bde8c0eacc944531a4a2189b835d2a97607aace0
Pablo <0101pablo@gmail.com>
2022-06-23 22:43:40 -0300

    replaced findViewById with viewBinding

--- app/src/main/java/com/packpack/pokedexdroid/view/MainActivity.kt
+++ app/src/main/java/com/packpack/pokedexdroid/view/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.packpack.pokedexdroid.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.packpack.pokedexdroid.R
+import com.packpack.pokedexdroid.databinding.ActivityMainBinding
 import com.packpack.pokedexdroid.domain.Pokemon
 import com.packpack.pokedexdroid.viewmodel.PokemonViewModel
 import com.packpack.pokedexdroid.viewmodel.PokemonViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var recyclerView: RecyclerView
     private val viewModel by lazy {
         ViewModelProvider(this, PokemonViewModelFactory())
             .get(PokemonViewModel::class.java)
     }
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        recyclerView = findViewById(R.id.rvPokemons)
-
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        recyclerView = binding.rvPokemons
         viewModel.pokemons.observe(this) {
             loadRecyclerView(it)
         }
--- app/src/main/java/com/packpack/pokedexdroid/view/PokemonAdapter.kt
+++ app/src/main/java/com/packpack/pokedexdroid/view/PokemonAdapter.kt
@@ -3,48 +3,43 @@ package com.packpack.pokedexdroid.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.packpack.pokedexdroid.R
+import com.packpack.pokedexdroid.databinding.PokemonItemBinding
 import com.packpack.pokedexdroid.domain.Pokemon
 
 class PokemonAdapter(private val pokemonList: List<Pokemon?>) :
-    RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
+    RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.pokemon_item, parent, false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        val itemBinding =
+            PokemonItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PokemonViewHolder(itemBinding)
     }
 
     override fun getItemCount() = pokemonList.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
         val pokemon = pokemonList[position]
 
         holder.bindView(pokemon)
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class PokemonViewHolder(private val itemBinding: PokemonItemBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
         fun bindView(pokemon: Pokemon?) = with(itemView) {
-            val ivPokemonImage = findViewById<ImageView>(R.id.ivPokemonImage)
-            val tvPokemonId = findViewById<TextView>(R.id.tvPokemonId)
-            val tvPokemonName = findViewById<TextView>(R.id.tvPokemonName)
-            val tvPokemonType1 = findViewById<TextView>(R.id.tvType1)
-            val tvPokemonType2 = findViewById<TextView>(R.id.tvType2)
-
             pokemon?.let { pokemon ->
-                Glide.with(itemView.context).load(pokemon.imageUrl).into(ivPokemonImage)
+                Glide.with(itemView.context).load(pokemon.imageUrl).into(itemBinding.ivPokemonImage)
                 val idText = "Nº ${pokemon.formattedId}"
-                tvPokemonId.text = idText
-                tvPokemonName.text = pokemon.formattedName
-                tvPokemonType1.text = pokemon.types[0].name.replaceFirstChar { it.uppercase() }
+                itemBinding.tvPokemonId.text = idText
+                itemBinding.tvPokemonName.text = pokemon.formattedName
+                itemBinding.tvType1.text = pokemon.types[0].name.replaceFirstChar { it.uppercase() }
                 if (pokemon.types.size > 1) {
-                    tvPokemonType2.visibility = View.VISIBLE
-                    tvPokemonType2.text = pokemon.types[1].name.replaceFirstChar { it.uppercase() }
+                    itemBinding.tvType2.visibility = View.VISIBLE
+                    itemBinding.tvType2.text =
+                        pokemon.types[1].name.replaceFirstChar { it.uppercase() }
                 } else {
-                    tvPokemonType2.visibility = View.GONE
+                    itemBinding.tvType2.visibility = View.GONE
                 }
             }
         }
